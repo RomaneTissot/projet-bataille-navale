6580812ad350a3bd11b6d8f52727b381acfcd535
Romane TISSOT <romane.tissot@centrale-med.fr>
2025-10-19 13:12:49 +0200

    Update test_bateau.py
    Ajustement des tests en conséquence de la modification de Bateau.positions()

--- test_bateau.py
+++ test_bateau.py
@@ -14,10 +14,10 @@ def test_horizontal():
 def test_positions_horizontal():
     b = Bateau(2, 3, longueur=3)
     expected = [(2, 3), (2, 4), (2, 5)]
-    assert b.positions() == expected
+    assert b.positions == expected
 
 
 def test_positions_vertical():
     b = Bateau(2, 3, longueur=3, vertical=True)
     expected = [(2, 3), (3, 3), (4, 3)]
-    assert b.positions() == expected
+    assert b.positions == expected
