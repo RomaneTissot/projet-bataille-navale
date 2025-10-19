fd93a18ed412dfb907ed9967001ec99165bd6ee2
Romane TISSOT <romane.tissot@centrale-med.fr>
2025-10-19 12:15:46 +0200

    Update story_grille.py
    User story "plouf dans l'eau" fonctionnelle pour tester la classe Grille

--- story_grille.py
+++ story_grille.py
@@ -1,13 +1,13 @@
-# from grille import Grille
+from grille import Grille
 
 
 print("=== Plouf dans l'eau ===")
-# g = Grille(5, 8)
+g = Grille(5, 8)
 
 continuer = True
 while continuer:
     print("\nGrille actuelle :")
-    # print(g)
+    print(g)
     saisie = input("\nEntrez les coordonnées du tir (x y), ou 'q' pour quitter : ")
 
     if saisie.lower() == 'q':
@@ -16,7 +16,7 @@ while continuer:
         try:
             x_str, y_str = saisie.split()
             x, y = int(x_str), int(y_str)
-            # g.tirer(x, y)
+            g.tirer(x, y)
         except ValueError:
             print("coordonées invalides. "
                   "Entrez deux entiers séparés par un espace (ex : 2 3).")
