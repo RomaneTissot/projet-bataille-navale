9fc8b2b92f14ab800c8f74f44242051fb8e1a90d
Romane TISSOT <romane.tissot@centrale-med.fr>
2025-10-19 11:50:53 +0200

    Update test_grille.py
    Ajout d'une fonction test du constructeur et légère modification du test précédent car le constructeur nécessite deux paramètres

--- test_grille.py
+++ test_grille.py
@@ -2,5 +2,13 @@ from grille import Grille
 
 
 def test_init():
-    g = Grille()
+    g = Grille(0, 0)
     assert isinstance(g, Grille)
+
+
+def test_grille_vide():
+    g = Grille(3, 4)
+    assert g.nb_lignes == 3
+    assert g.nb_colonnes == 4
+    assert len(g.grille) == 12
+    assert all(c == g.vide for c in g.grille)
