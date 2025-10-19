932f025badb3f9dd023bf1776beddfaae73da78e
Romane TISSOT <romane.tissot@centrale-med.fr>
2025-10-19 13:01:55 +0200

    Update test_bateau.py
    Tests pour la position d'un bateau horizontal ou vertical

--- test_bateau.py
+++ test_bateau.py
@@ -9,3 +9,15 @@ def test_longueur():
 def test_horizontal():
     b = Bateau(2, 3)
     assert b.vertical is False
+
+
+def test_positions_horizontal():
+    b = Bateau(2, 3, longueur=3)
+    expected = [(2, 3), (2, 4), (2, 5)]
+    assert b.positions() == expected
+
+
+def test_positions_vertical():
+    b = Bateau(2, 3, longueur=3, vertical=True)
+    expected = [(2, 3), (3, 3), (4, 3)]
+    assert b.positions() == expected
