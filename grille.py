f06dbcbbcdea1ead8d7d281e6c340e71b715dc98
Romane TISSOT <romane.tissot@centrale-med.fr>
2025-10-19 11:49:38 +0200

    Update grille.py
    Ajout du constructeur permettant d'initialiser une grille vide

--- grille.py
+++ grille.py
@@ -1,2 +1,7 @@
 class Grille:
-    pass
+    def __init__(self, nb_lignes, nb_colonnes):
+        self.nb_lignes = nb_lignes
+        self.nb_colonnes = nb_colonnes
+        self.vide = '∿'
+        self.touche = 'x'
+        self.grille = [self.vide] * (nb_lignes * nb_colonnes)
