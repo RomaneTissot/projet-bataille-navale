55fa86007961d32f65856ef9cc97ca6e730bbb59
Romane TISSOT <romane.tissot@centrale-med.fr>
2025-10-19 14:21:51 +0200

    Update bateau.py
    Ajout de la méthode Bateau.coule qui vérifie si un bateau est coulé ou non

--- bateau.py
+++ bateau.py
@@ -15,3 +15,10 @@ class Bateau:
             for i in range(self.longueur):
                 pos.append((self.ligne, self.colonne + i))
         return pos
+
+    def coule(self, grille):
+        for (l, c) in self.positions:
+            indice = l * grille.nb_colonnes + c
+            if grille.grille[indice] != 'x':
+                return False
+        return True
