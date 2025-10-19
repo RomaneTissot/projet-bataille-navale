ee04f6968490e9d09e7ed8f182dedd622b23fe95
Romane TISSOT <romane.tissot@centrale-med.fr>
2025-10-19 12:04:55 +0200

    Update test_grille.py
    Test de la méthode grille.tirer(nb_lines, nbb_colonnes)

--- test_grille.py
+++ test_grille.py
@@ -12,3 +12,9 @@ def test_grille_vide():
     assert g.nb_colonnes == 4
     assert len(g.grille) == 12
     assert all(c == g.vide for c in g.grille)
+
+
+def test_tirer():
+    g = Grille(3, 3)
+    g.tirer(1, 2)
+    assert g.grille[1 * 3 + 2] == g.touche
